55521b95b851c71339c672a0372b081b53d9056c
myrionsc <marpht23@gmail.com>
2017-09-16 16:44:41 +0200

    added persistence: loading of datafile on start and method for saving to file

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,18 +1,36 @@
 const fs = require('fs');
 
 export class Utils {
-    public static saveFile() {
-        if (!fs.existsSync(__dirname + "/data")) {
-            fs.mkdir(__dirname + "/data", (err: Error) => {
-                if (err) return console.log(err.message)
-            });
-        }
+    public static saveData(short: Array<string>, long: Array<string>) {
+        let obj = {
+            "short": short,
+            "long": long
+        };
+        let data = JSON.stringify(obj);
 
-        fs.writeFile(__dirname + "/data/test.txt", "Hey there!", (err: Error) => {
+        fs.writeFile(__dirname + "/data.json", data, (err: Error) => {
             if (err) {
-                return console.log(err.message);
+                return console.error(err.message);
             }
             console.log("write succesful");
         });
     }
+
+    public static readDataFile(callback: Function) {
+        fs.readFile(__dirname + "/data.json", 'utf8', (err: Error, data: string) => {
+            if (err) {
+                console.error(err.message);
+                data = '{ "short": [], "long": [] }';
+                callback(data);
+            }
+            callback(data);
+        });
+    }
 }
+
+
+// if (!fs.existsSync(__dirname + "/data")) {
+//     fs.mkdir(__dirname + "/data", (err: Error) => {
+//         if (err) return console.log(err.message)
+//     });
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 #!/usr/bin/env node
+let express = require('express');
+let app = express();
 
 import { Utils } from './Utils';
 
-Utils.saveFile();
+let short: string[] = [];
+let long: string[] = [];
+
+// --- startup
+// evaluate data if it exists
+Utils.readDataFile((data: string) => {
+    let dataobj = JSON.parse(data);
+    console.log("Data loaded:");
+    console.log(dataobj);
+
+    short = dataobj.short;
+    long = dataobj.long;
+});
+// Utils.saveData(short, long);
+
+
+
+
+// --- rest api
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
 
