1ef65eb498039a939027d9f0e72cb46fac05d406
myrionsc <marpht23@gmail.com>
2018-02-14 13:36:58 +0100

    updated packages to angular 5 and added service worker

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {DataService} from "./shared/services/data.service";
 import {HttpClientModule} from "@angular/common/http";
-import { TodoComponent } from './todo/todo.component';
+import {TodoComponent} from './todo/todo.component';
+import {ServiceWorkerModule} from "@angular/service-worker";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [DataService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodoComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ServiceWorkerModule,
+        HttpClientModule
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
