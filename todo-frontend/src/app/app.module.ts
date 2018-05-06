f545e516748f9b7c32c4434f7b1c8a3119bdc13b
myrionsc <marpht23@gmail.com>
2018-05-06 17:41:00 +0200

    generated navmenu and table

--- todo-frontend/src/app/app.module.ts
+++ todo-frontend/src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MyTableComponent } from './my-table/my-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MyTableComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
