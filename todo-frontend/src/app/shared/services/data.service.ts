9e8a6fcc655d47075be173468bc01d7dc9d5af0d
myrionsc <marpht23@gmail.com>
2018-05-13 17:34:56 +0200

    light refactor

--- todo-frontend/src/app/shared/services/data.service.ts
+++ todo-frontend/src/app/shared/services/data.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DataService {
     url = environment.hostUrl;
+    private httpOptions: any = {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+        responseType: 'text'
+    };
 
     constructor(private httpClient: HttpClient) {
         console.log(environment.hostUrl);
@@ -17,48 +21,27 @@ export class DataService {
         return this.httpClient.get(this.url + 'api/list');
     }
 
-    // todo: try to remove options object. I think it was fixed in 6
     addPersonal(str: string) {
-        return this.httpClient.post(this.url + 'api/personal/', {"item": str},
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.post(this.url + 'api/personal/', {"item": str}, this.httpOptions);
     }
+
     addProfessional(str: string) {
-        return this.httpClient.post(this.url + 'api/professional/', {"item": str},
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.post(this.url + 'api/professional/', {"item": str}, this.httpOptions);
     }
+
     addShopping(str: string) {
-        return this.httpClient.post(this.url + 'api/shopping/', {"item": str},
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.post(this.url + 'api/shopping/', {"item": str}, this.httpOptions);
     }
 
     deletePersonal(n: number) {
-        return this.httpClient.delete(this.url + 'api/personal/' + n,
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.delete(this.url + 'api/personal/' + n, this.httpOptions);
     }
+
     deleteProfessional(n: number) {
-        return this.httpClient.delete(this.url + 'api/professional/' + n,
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.delete(this.url + 'api/professional/' + n, this.httpOptions);
     }
+
     deleteShopping(n: number) {
-        return this.httpClient.delete(this.url + 'api/shopping/' + n,
-            {
-                headers: new HttpHeaders().set('Content-Type', 'application/json'),
-                responseType: 'text'
-            });
+        return this.httpClient.delete(this.url + 'api/shopping/' + n, this.httpOptions);
     }
 }
