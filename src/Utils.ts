0d08af605b300615db1478397309000433a96a5e
myrionsc <marpht23@gmail.com>
2018-05-13 21:21:10 +0200

    saves and gets from file every operation

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,11 +1,18 @@
 const fs = require('fs');
 
-export class Utils {
-    public static saveData(personal: string[], professional: string[], shopping: string[], callback?: Function) {
-        let obj = this.createDataObj(personal, professional, shopping);
-        let data = JSON.stringify(obj);
+export class Db { // not really tho :P
+    public static getData(callback: Function) {
+        fs.readFile(__dirname + "/data.json", 'utf8', (err: Error, data: string) => {
+            if (err) {
+                console.error(err.message);
+                data = '{ "personal": [], "professional": [], "shopping": [] }';
+            }
+            callback(JSON.parse(data));
+        });
+    }
 
-        fs.writeFile(__dirname + "/data.json", data, (err: Error) => {
+    public static saveData(data: any, callback?: Function) {
+        fs.writeFile(__dirname + "/data.json", JSON.stringify(data), (err: Error) => {
             if (err) {
                 return console.error(err.message);
             }
@@ -16,21 +23,10 @@ export class Utils {
         });
     }
 
-    public static readDataFile(callback: Function) {
-        fs.readFile(__dirname + "/data.json", 'utf8', (err: Error, data: string) => {
-            if (err) {
-                console.error(err.message);
-                data = '{ "personal": [], "professional": [], "shopping": [] }';
-            }
-            callback(data);
+    public static modifyData(modifyFunc: Function) {
+        this.getData((data: any) => {
+            let modData = modifyFunc(data);
+            this.saveData(modData);
         });
-    }
-
-    public static createDataObj(personal: string[], professional: string[], shopping: string[]) {
-        return {
-            "personal": personal,
-            "professional": professional,
-            "shopping": shopping
-        };
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,142 +2,134 @@
 import express = require('express');
 import bodyParser = require('body-parser');
 import path = require('path');
+
 let app = express();
 
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
     next();
 });
 
-import { Utils } from './Utils';
+import {Db as db} from './Utils';
 import {isNullOrUndefined} from "util";
 
-let personal: string[] = [];
-let professional: string[] = [];
-let shopping: string[] = [];
-
-// --- startup
-// read file and parse data if it exists
-Utils.readDataFile((data: string) => {
-    let dataobj = JSON.parse(data);
-    console.log("Data loaded:");
-    console.log(dataobj);
-
-    personal = dataobj.personal;
-    professional = dataobj.professional;
-    shopping = dataobj.shopping;
-});
-
 // --- rest api
-app.get('/api/list', function(req: express.Request, res: express.Response){
-    let dataobj = Utils.createDataObj(personal, professional, shopping);
-    console.log("Sent data: " + JSON.stringify(dataobj));
-    res.send(dataobj);
+app.get('/api/list', function (req: express.Request, res: express.Response) {
+    db.getData((data: any) => {
+        console.log("Sent data: " + JSON.stringify(data));
+        res.send(data);
+    })
 });
-app.get('/api/personal', function(req: express.Request, res: express.Response){
-    console.log("Sent data: " + JSON.stringify(personal));
-    res.send(personal);
+app.get('/api/personal', function (req: express.Request, res: express.Response) {
+    db.getData((data: any) => {
+        console.log("Sent data: " + JSON.stringify(data.personal));
+        res.send(data);
+    })
 });
-app.get('/api/professional', function(req: express.Request, res: express.Response){
-    console.log("Sent data: " + JSON.stringify(professional));
-    res.send(professional);
+app.get('/api/professional', function (req: express.Request, res: express.Response) {
+    db.getData((data: any) => {
+        console.log("Sent data: " + JSON.stringify(data.professional));
+        res.send(data);
+    })
 });
-app.get('/api/shopping', function(req: express.Request, res: express.Response){
-    console.log("Sent data: " + JSON.stringify(shopping));
-    res.send(shopping);
+app.get('/api/shopping', function (req: express.Request, res: express.Response) {
+    db.getData((data: any) => {
+        console.log("Sent data: " + JSON.stringify(data.shopping));
+        res.send(data);
+    })
 });
 
-app.post('/api/personal', function(req: express.Request, res: express.Response){
+app.post('/api/personal', function (req: express.Request, res: express.Response) {
     let item: string = req.body.item;
     if (item && typeof item === "string") {
-        console.log("item received at /api/personal: " + item);
-        personal.push(item);
-        Utils.saveData(personal, professional, shopping, () => {
-            res.sendStatus(200);
+        db.modifyData((data: any) => {
+            data.personal.push(item);
+            return data;
         });
+        res.sendStatus(200);
     } else {
         console.log("bad request received at /api/personal");
         res.status(400).send('Bad request. data format should be: {"item": "string"}');
     }
 });
-app.post('/api/professional', function(req: express.Request, res: express.Response){
-    let item: string = req.body.item;
-    if (item && typeof item === "string") {
-        console.log("item received at /api/professional: " + item);
-        professional.push(item);
-        Utils.saveData(personal, professional, shopping, () => {
-            res.sendStatus(200);
-        });
-    } else {
-        console.log("bad request received at /api/professional");
-        res.status(400).send('Bad request. data format should be: {"item": "string"}');
-    }
-});
-app.post('/api/shopping', function(req: express.Request, res: express.Response){
-    let item: string = req.body.item;
-    if (item && typeof item === "string") {
-        console.log("item received at /api/shopping: " + item);
-        shopping.push(item);
-        Utils.saveData(personal, professional, shopping, () => {
-            res.sendStatus(200);
-        });
-    } else {
-        console.log("bad request received at /api/shopping");
-        res.status(400).send('Bad request. data format should be: {"item": "string"}');
-    }
-});
-
-app.delete('/api/personal/:pos', function(req: express.Request, res: express.Response){
-    let pos = Number(req.params.pos);
-    if (!isNullOrUndefined(pos) && typeof pos === 'number') {
-        console.log("delete request recieved for /api/personal: " + pos)
-        let deletedItem = personal.splice(pos, 1)[0];
-        if (deletedItem) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    } else {
-        console.log("bad delete request recieved for /api/personal");
-        res.status(400).send('Bad request. data format should be: {"pos": 1}');
-    }
-});
-app.delete('/api/professional/:pos', function(req: express.Request, res: express.Response){
-    let pos = Number(req.params.pos);
-    if (!isNullOrUndefined(pos) && typeof pos === 'number') {
-        console.log("delete request recieved for /api/professional: " + pos)
-        let deletedItem = professional.splice(pos, 1)[0];
-        if (deletedItem) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    } else {
-        console.log("bad delete request recieved for /api/professional");
-        res.status(400).send('Bad request. data format should be: {"pos": 1}');
-    }
-});
-app.delete('/api/shopping/:pos', function(req: express.Request, res: express.Response){
-    let pos = Number(req.params.pos);
-    if (!isNullOrUndefined(pos) && typeof pos === 'number') {
-        console.log("delete request recieved for /api/shopping: " + pos)
-        let deletedItem = shopping.splice(pos, 1)[0];
-        if (deletedItem) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    } else {
-        console.log("bad delete request recieved for /api/shopping");
-        res.status(400).send('Bad request. data format should be: {"pos": 1}');
-    }
-});
+// app.post('/api/professional', function (req: express.Request, res: express.Response) {
+//     let item: string = req.body.item;
+//     if (item && typeof item === "string") {
+//         console.log("item received at /api/professional: " + item);
+//         professional.push(item);
+//         db.saveData(personal, professional, shopping, () => {
+//             res.sendStatus(200);
+//         });
+//     } else {
+//         console.log("bad request received at /api/professional");
+//         res.status(400).send('Bad request. data format should be: {"item": "string"}');
+//     }
+// });
+// app.post('/api/shopping', function (req: express.Request, res: express.Response) {
+//     let item: string = req.body.item;
+//     if (item && typeof item === "string") {
+//         console.log("item received at /api/shopping: " + item);
+//         shopping.push(item);
+//         db.saveData(personal, professional, shopping, () => {
+//             res.sendStatus(200);
+//         });
+//     } else {
+//         console.log("bad request received at /api/shopping");
+//         res.status(400).send('Bad request. data format should be: {"item": "string"}');
+//     }
+// });
+//
+// app.delete('/api/personal/:pos', function (req: express.Request, res: express.Response) {
+//     let pos = Number(req.params.pos);
+//     if (!isNullOrUndefined(pos) && typeof pos === 'number') {
+//         console.log("delete request recieved for /api/personal: " + pos)
+//         let deletedItem = personal.splice(pos, 1)[0];
+//         if (deletedItem) {
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(404);
+//         }
+//     } else {
+//         console.log("bad delete request recieved for /api/personal");
+//         res.status(400).send('Bad request. data format should be: {"pos": 1}');
+//     }
+// });
+// app.delete('/api/professional/:pos', function (req: express.Request, res: express.Response) {
+//     let pos = Number(req.params.pos);
+//     if (!isNullOrUndefined(pos) && typeof pos === 'number') {
+//         console.log("delete request recieved for /api/professional: " + pos)
+//         let deletedItem = professional.splice(pos, 1)[0];
+//         if (deletedItem) {
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(404);
+//         }
+//     } else {
+//         console.log("bad delete request recieved for /api/professional");
+//         res.status(400).send('Bad request. data format should be: {"pos": 1}');
+//     }
+// });
+// app.delete('/api/shopping/:pos', function (req: express.Request, res: express.Response) {
+//     let pos = Number(req.params.pos);
+//     if (!isNullOrUndefined(pos) && typeof pos === 'number') {
+//         console.log("delete request recieved for /api/shopping: " + pos)
+//         let deletedItem = shopping.splice(pos, 1)[0];
+//         if (deletedItem) {
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(404);
+//         }
+//     } else {
+//         console.log("bad delete request recieved for /api/shopping");
+//         res.status(400).send('Bad request. data format should be: {"pos": 1}');
+//     }
+// });
 
 app.listen(8080, () => {
     console.log("App listening on port 8080");
